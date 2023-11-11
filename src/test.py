d54097e472578a56632a922980648d3e4e1d0cc8
meer-khan <shahmirkhan519@gmail.com>
2023-11-11 11:29:53 +0500

    working on a solution to find the encoding and apply that enconding

--- src/test.py
+++ src/test.py
@@ -1,16 +1,41 @@
-def read_file(file:str) -> str:
-    data = ''
-    with open(file, 'r', encoding='utf-8 LE') as file: 
-        data = file.read()
-        # print(data)
-        # print(type(data))
-    return data 
+# def read_file(file:str) -> str:
+#     data = ''
+#     with open(file, 'r', encoding='utf-16 LE') as file: 
+#         data = file.read()
+#         # print(data)
+#         # print(type(data))
+#     return data 
 
-def read_packages(data):
-    splitted_data = data.split("\n")
-    non_version_packages = set()
-    packages = [{i[0]:i[1]} for i in [i.split("==") if "==" in i else non_version_packages.add(i) for i in splitted_data] if i is not None]
-    return packages
+
+import chardet
+
+def read_file(file_path):
+    # Detect the encoding of the file
+    with open(file_path, 'rb') as file:
+        result = chardet.detect(file.read())
+
+    # Use the detected encoding to read the file
+    encoding = result['encoding']
+    confidence = result['confidence']
+
+    if confidence > 0.5:  # You can adjust the confidence threshold as needed
+        with open(file_path, 'r', encoding=encoding) as file:
+            content = file.read()
+        return content
+    else:
+        print(f"Low confidence in detected encoding ({confidence}). Manual inspection may be needed.")
+
+# Example usage
+file_path = 'D:\\2022\\Python-DCV\\test_data\\test_directory_1\\src\\requirements_2.txt'
+file_content = read_file(file_path)
+print(file_content)
+
+
+# def read_packages(data):
+#     splitted_data = data.split("\n")
+#     non_version_packages = set()
+#     packages = [{i[0]:i[1]} for i in [i.split("==") if "==" in i else non_version_packages.add(i) for i in splitted_data] if i is not None]
+#     return packages
 
 # data1 = read_file(r"D:\2022\Python-DCV\test_data\requirements_2.txt")
 data2 = read_file(r"D:\\2022\\Python-DCV\\test_data\\test_directory_1\\src\\requirements_2.txt")
