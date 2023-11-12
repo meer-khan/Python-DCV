a20696fea7c5f8cb62058c160eca4971f6094d99
meer-khan <shahmirkhan519@gmail.com>
2023-11-12 20:33:35 +0500

    added a new variation of import

--- test_data/test_directory_1/src/imports.py
+++ test_data/test_directory_1/src/imports.py
@@ -1,9 +1,30 @@
 import sys
-import os
-import pathlib
+import re
+import pathlib ,os
 from fastapi import FastAPI, Depends, Response, status
 from typing import Union
 from typing_extensions import Annotated
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from spdx_tools.common.spdx_licensing import spdx_licensing
+from spdx_tools.spdx.model import (
+    Actor,
+    ActorType,
+    Checksum,
+    ChecksumAlgorithm,
+    CreationInfo,
+    Document,
+    ExternalPackageRef,
+    ExternalPackageRefCategory,
+    File,
+    FileType,
+    Package,
+    PackagePurpose,
+    PackageVerificationCode,
+    Relationship,
+    RelationshipType,
+)
+from spdx_tools.spdx.validation.document_validator import validate_full_spdx_document
+from spdx_tools.spdx.validation.validation_message import ValidationMessage
+from spdx_tools.spdx.writer.write_anything import write_file
