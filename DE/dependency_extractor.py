79401e05084007e450f66941838dc6bee0d88b1b
meer-khan <shahmirkhan519@gmail.com>
2023-11-18 18:05:37 +0500

    added regex of multi imports

--- DE/dependency_extractor.py
+++ DE/dependency_extractor.py
@@ -19,7 +19,7 @@ import ast
 4. Extract from single import e.g. from pandas import read_csv  # TODO: ^from\s+(\w+)\s+import\s+(\w+)$
     4.1 Extract package name e.g. pandas
     4.2 Extract function/class/module used by from import e.g. read_csv 
-5. Extract from multiple import e.g. from pandas import read_csv, DataFrame, to_csv  
+5. Extract from multiple import e.g. from pandas import read_csv, DataFrame, to_csv   #TODO: ^from\s+\w+\s+import\s+\w+\s*,.*$
     5.1 Extract package name e.g. pandas 
     5.1 Extract function/class/module used by from import e.g. read_csv, DataFrame, to_csv 
 6. Extract from multi package multi class e.g. from flaskapi.sqlalchemy.request import sqlalchemy_request
